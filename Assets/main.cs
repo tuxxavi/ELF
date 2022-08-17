8c1f68b0deaf8c2e2176f89431bf7abade40d328
xavi.guillorme <xavi.guillorme@zitrogames.com>
2022-08-18 00:09:55 +0200

    PETAAAA

--- Assets/main.cs
+++ Assets/main.cs
@@ -1,16 +1,74 @@
 using System;
+using System.Collections.Generic;
 using CometEngine;
+using CometEngine.Json;
+
+class Player
+{
+	public int Id;
+	public String Name;
+}
+
+class Team
+{
+	public String Name;
+	public List<Player> Players = new List<Player>();
+}
 
 class main : CometBehaviour
 {
+	private List<Team> mTeams = new List<Team>();
+
 	// Called before first frame
 	public void Start()
 	{
 		Debug.Log("Empezamos");
+		JsonObject data = JsonObject.FileToJson(App.dataAssetsPath + "Game.json");
+		if (data != null)
+		{
+			JsonArray PlayersJSON = data.GetArray("players");
+			JsonArray TeamsJSON = data.GetArray("equipos");
+			for (uint i=0; i<TeamsJSON.GetSize(); i++)
+			{
+				JsonObject team = TeamsJSON.GetObject(i);
+				if (team != null)
+				{
+					Team Data = new Team();
+					Data.Name = team.GetString("name");
+					int[] Players = team.GetIntArray("players");
+					for (uint j=0; j<Players.Length; j++)
+					{
+						JsonObject info = PlayersJSON.GetObject((uint)Players[j]);
+						Player player = new Player();
+						player.Id = info.GetInt("id");
+						player.Name = info.GetString("name");
+						Data.Players.Add(player);
+					}
+					mTeams.Add(Data);
+				}
+			}
+		}
+		data.Free();
+
+		
+		for (int i =0; i<mTeams.Count; i++)
+		{
+			AddPlayersInScene(i);
+		}
 	}
 
 	// Called Every Frame
 	public void Update()
 	{
+	}
+	public void AddPlayersInScene(int IdTeam)
+	{
+		List<Player> Players =  mTeams[IdTeam].Players;
+		for (int j =0; j<Players.Count; j++)
+		{
+			GameObject PlayerScene = RuntimeAssets.LoadGameObject("scenes/player");
+			//PlayerScene.name = Players[j].Name;
+			CometEngine.Object.Instantiate(PlayerScene);
+		}
 	}
 }
